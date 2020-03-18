8eabb5622cf20a1f81df51c6d611768e8a04dad8
Giguru <giguru.scheuer@gmail.com>
2020-03-18 21:37:07 +0100

    Added more content

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharetype=teams&member=3899512&utm_medium=social&utm_source=whatsapp&utm_campaign=p_na+share-sheet&rcid=db09abb2748b48ff8e97253880401149";
+$aanmeldLink = "";
+$bedrijvenLink = "";
+$infoEmail = "info@departicipatiekeuken.nl";
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +15,7 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
 <body>
     <header>
         <nav class="navbar fixed-top navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#"></a>
+            <a target="_blank" class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -20,16 +23,16 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo $donateLink; ?>">Doneer</a>
+                        <a target="_blank" class="nav-link" href="<?php echo $donateLink; ?>">Doneer</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#offer-help">Ik wil helpen!</a>
+                        <a target="_blank" class="nav-link" href="#offer-help">Ik wil helpen!</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#join-the-team">Join the team</a>
+                        <a target="_blank" class="nav-link" href="#join-the-team">Join the team</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
+                        <a target="_blank" class="nav-link" href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -37,53 +40,100 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
     </header>
     <section class="container-fluid" id="banner">
         <div class="container">
-            <div class="col-sm-6">
-                <h1>100.000 maaltijden</h1>
-                <p class="font-weight-bold">
-                    De Participatie Keuken zet zich om voor eenzame ouderen 15 weken langs elke dag een diepvriesmaaltijd te bereiden in de regio Den Haag.
-                </p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>100.000 maaltijden</h1>
+                    <p class="font-weight-bold">
+                        De Participatie Keuken zet zich om voor eenzame ouderen 15 weken langs elke dag een diepvriesmaaltijd te bereiden in de regio Den Haag.
+                    </p>
+                </div>
             </div>
         </div>
     </section>
-    <section class="text-center">
-        <h4>Verhaal van Ben</h4>
-        <quote>&quot;Ik hoop dat we hier sterker uit komen. Onze krachten bundelen, daar gaat het om!&quot;</quote>
+    <section class="text-center container" id="offer-help">
+        <h2>Ik wil helpen!</h2>
+        <p class="subtitle">Hoe kan ik helpen?</p>
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="image-block" src="assets/images/volunteer-icon.png" />
+                <h3>Word vrijwilliger</h3>
+                <p class="text-justify">
+                    De ambitie van Ben om 100.000 maaltijden te bereiden is niet niks, maar vele handen maken licht werk! De Participatie Keuken is daarom opzoek naar vrijwilligers die in allerlei facetten van het project een steentje willen bijdragen. Meld je snel aan en geef aan waar wij jouw hulp kunnen gebruiken!
+                </p>
+                <a target="_blank" class="btn btn-lg btn-red">
+                    Meld je hier aan
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <div class="image-block"></div>
+                <h3>Word Donateur</h3>
+                <p class="text-justify">
+                    Met behulp van jouw donatie kunnen wij het plan verwezenlijken om voor eenzame ouderen diepvriesmaaltijden te bereiden en deze vervolgens bij hen te kunnen langs brengen! Denk bijvoorbeeld aan het inkopen van alle ingrediënten voor de recepten, labels met de bereidingswijze, maar ook het vervoeren van de maaltijden.
+                </p>
+                <a target="_blank" class="btn btn-lg btn-bordeaux" href="<?php echo $donateLink; ?>">
+                    Klik hier om te doneren
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <img class="image-block" src="assets/images/bedrijven-icon.png" />
+                <h3>Bedrijven</h3>
+                <p class="text-justify">
+                    Als bedrijf bent u onmisbaar in het succes van de Participatie Keuken. Momenteel staan wij voor vele uitdagingen waar wij kennis, producten, ideeën en mensen vanuit het bedrijfsleven hard kunnen gebruiken.
+                </p>
+                <a target="_blank" class="btn btn-lg btn-blue">
+                    Bedrijven
+                </a>
+            </div>
+        </div>
     </section>
-    <section class="text-center" id="offer-help">
-        <div class="container">
-            <h2>Ik wil helpen!</h2>
-            <p class="subtitle">Hoe kan ik helpen?</p>
-            <div class="row">
-                <div class="col-sm-4">
-                    <img class="image-block" src="assets/images/volunteer-icon.png" />
-                    <h3>Word vrijwilliger</h3>
-                    <p class="text-justify">
-                        De ambitie van Ben om 100.000 maaltijden te bereiden is niet niks, maar vele handen maken licht werk! De Participatie Keuken is daarom opzoek naar vrijwilligers die in allerlei facetten van het project een steentje willen bijdragen. Meld je snel aan en geef aan waar wij jouw hulp kunnen gebruiken!
-                    </p>
-                    <a class="btn btn-lg btn-red">
-                        Meld je hier aan
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <div class="image-block"></div>
-                    <h3>Word Donateur</h3>
-                    <p class="text-justify">
-                        Met behulp van jouw donatie kunnen wij het plan verwezenlijken om voor eenzame ouderen diepvriesmaaltijden te bereiden en deze vervolgens bij hen te kunnen langs brengen! Denk bijvoorbeeld aan het inkopen van alle ingrediënten voor de recepten, labels met de bereidingswijze, maar ook het vervoeren van de maaltijden.
-                    </p>
-                    <a class="btn btn-lg btn-bordeaux" href="<?php echo $donateLink; ?>">
-                        Klik hier om te doneren
-                    </a>
-                </div>
-                <div class="col-sm-4">
-                    <img class="image-block" src="assets/images/bedrijven-icon.png" />
-                    <h3>Bedrijven</h3>
-                    <p class="text-justify">
-                        Als bedrijf bent u onmisbaar in het succes van de Participatie Keuken. Momenteel staan wij voor vele uitdagingen waar wij kennis, producten, ideeën en mensen vanuit het bedrijfsleven hard kunnen gebruiken.
-                    </p>
-                    <a class="btn btn-lg btn-blue">
-                        Bedrijven
-                    </a>
-                </div>
+    <section class="text-justify container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h3>Het verhaal van Ben</h3>
+                <p>
+                    Gewoon door samen te koken en daarna aan tafel te gaan bracht Ben Lachhab eenzame ouderen bij elkaar. Toen dit niet meer mogelijk was, ging Ben gelijk aan de slag.
+                </p>
+                <p>
+                    Honderdduizend maaltijden voor kwetsbare ouderen wilde hij uitdelen voor al diegenen die nu nergens meer terecht kunnen. Waar hij alleen startte met een bericht op Facebook wordt er op dit moment al met vele partijen besproken hoe dit plan volgende week succesvol van start kan gaan.
+                </p>
+                <blockquote>&quot;Ik hoop dat we hier sterker uit komen. Onze krachten bundelen, daar gaat het om!&quot;</blockquote>
+                <p>
+                    En dat is gebeurd! Shell heeft inmiddels de katering beschikbaar gesteld om dit project te kunnen waarmaken. Verder is gemeente Den Haag ook nauw betrokken om van dit project een succes te maken!
+                </p>
+            </div>
+            <div class="col-sm-6" id="news-block">
+                <h3>Ben in het Nieuws</h3>
+                <ul class="news-items">
+                    <li>
+                        <span class="datum">19:45 18 maart</span>
+                        <p>
+                            Het initiatief van de Haagse restaurant manager Ben Lachhab wordt door steeds meer bedrijven gesteund. Zo kookte het hoofdkantoor van Shell woensdag al haar voorraad voor het bedrijfsrestaurant tot diepvriesmaaltijden. 'Toen we van de actie hoorden, werden we gelijk enthousiast. Dit is een ontzettend mooi initiatief en op deze manier verspillen we geen eten', zegt Michiel van Wolferen, hospitality manager bij Shell.
+                            <a target="_blank" href="https://www.omroepwest.nl/nieuws/4014114/Shell-kookt-diepvriesmaaltijden-voor-kwetsbare-ouderen">
+                                Lees het hele artikel hier.
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                        <span class="datum">12:30 18 maart</span>
+                        <p>
+                            Hagenaar Ben brengt duizenden maaltijden thuis bij kwetsbare ouderen
+                            Ben Lachhab bezorgt de komende vijftien weken dankzij giften van sponsoren 100.000 maaltijden bij kwetsbare ouderen. &quot;Uit angst voor het coronavirus durven zij vaak de deur niet uit. En ze hebben toch heus eten nodig.&quot;
+                            <a target="_blank" href="https://www.ad.nl/den-haag/hagenaar-ben-brengt-duizenden-maaltijden-thuis-bij-kwetsbare-ouderen~ab2cee0e/">
+                                Lees het hele artikel hier.
+                            </a>
+                        </p>
+                    </li>
+                    <li>
+                    <span class="datum">17:30 14 maart
+</span>
+                        <p>
+                            DEN HAAG - Honderdduizend maaltijden om duizend kwetsbare ouderen vijftien weken lang van een maaltijd te voorzien, voor het geval dat Nederland op slot gaat vanwege het coronavirus. Hagenaar Ben Lachhab is druk met het uitwerken van zijn plan. 'We moeten nu bewijzen dat we er als maatschappij voor elkaar zijn.
+                            <a target="_blank" href="https://www.omroepwest.nl/nieuws/4011604/Hagenaar-wil-tijdens-coronacrisis-100-000-maaltijden-invriezen-voor-ouderen">
+                                Lees het hele artikel hier.
+                            </a>
+                        </p>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
@@ -91,14 +141,10 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
         <div class="container">
             <div class="row">
                 <div class="col-sm-4">
-                    <h2>Wij zoeken jou!</h2>
-                    <p>Voor ons initiatief zijn wij opzoek naar
-                    Wij zijn op zoek naar mensen die kunnen helpen voor het succes van de Participatie keuken. Hiervoor hebben
-                    wij ook specifieke hulp voor nodig!
-                    </p>
-                    <p class="font-weight-bold">
-                        Ben of ken jij de persoon die we zoeken? Stuur dan een mail naar&nbsp;
-                        <a href="mailto:team@departicipatiekeuken.nl">team@departicipatiekeuken.nl</a>
+                    <h2>Join the team</h2>
+                    <p>
+                        Wij zijn op zoek naar mensen die kunnen helpen om van de Participatie keuken een groot succes te maken. Hiervoor hebben wij ook specifieke hulp voor nodig! Wij zijn momenteel op zoek naar vrijwilligers die zich willen inzetten als de rechterhand van Ben, de initiatiefnemer. Je zal je bezig houden met zowel organisatorische zaken als de communicatie. Ben of ken jij de persoon die we zoeken? Stuur dan een mail naar
+                        <a target="_blank" class="font-weight-bold" href="mailto:<?php echo $infoEmail; ?>"><?php echo $infoEmail; ?></a>
                     </p>
                 </div>
                 <div class="col-sm-8">
@@ -114,18 +160,35 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
             </div>
         </div>
     </section>
-    <section>
+    <footer>
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
                     <h3>Over ons</h3>
+                    <p>
+                        Binnen een dag wist Ben een heel team om zich heen te verzamelen. De groep bestaat uit een divers team met verschillende ervaringen. <i>Jan Paul</i> zal als kok van de maaltijden zich richten op alle inkoop van de ingrediënten tot aan de apparatuur. <i>Liesbeth</i> gaat samen met <i>Max, Gert-Jan, Sophie</i> en <i>Maxime</i> aan de slag met de logistiek en communicatie van het initiatief.
+                    </p>
+                    <p>
+                        Natuurlijk kunnen wij hulp gebruiken, dus <a target="_blank" href="mailto:<?php echo $infoEmail; ?>">meld je aan!</a>
+                    </p>
                 </div>
                 <div class="col-sm-6" id="contact">
                     <h3>Contact</h3>
+                    <p>
+                        <span class="font-weight-bold">Algemeen</span>
+                        <br/>
+                        Email: <a target="_blank" href="mailto:<?php echo $infoEmail; ?>"><?php echo $infoEmail; ?></a>
+                    </p>
+
+                    <p>
+                        <span class="font-weight-bold">Logistiek</span>
+                        <br/>
+                        Email: <a target="_blank" href="mailto:actieteam@participatiekeuken.nl">actieteam@participatiekeuken.nl</a>
+                    </p>
                 </div>
             </div>
         </div>
-    </section>
+    </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
     <script src="js/bootstrap.min.js"></script>
     <script>
