f96db1e738c8f897ae1cbdfaa3f65208684f0ffd
Giguru <giguru.scheuer@gmail.com>
2020-03-20 15:13:06 +0100

    Added LinkedIn and changed email

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $donateLink = "https://www.gofundme.com/f/100000-maaltijden-voor-ouderen?sharety
 $aanmeldLink = "https://docs.google.com/forms/d/17UjKk42-lABFXCo8DTdsNRWUjSNF0vwg6TpVr2i7KJg/edit?usp=sharing";
 $bedrijvenLink = "https://docs.google.com/forms/d/1bVtzks3fCWgRn-WZvFnPkgUmid7GSNee1PvNRpAmjx0/viewform?edit_requested=true";
 $infoEmail = "info@participatiekeuken.nl";
+$actieTeamEmail = "actieteamparticipatiekeuken@gmail.com";
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,123 +51,125 @@ $infoEmail = "info@participatiekeuken.nl";
             </div>
         </nav>
     </header>
-    <section class="container-fluid banner" id="banner">
-        <div class="container">
+    <main>
+        <section class="container-fluid banner" id="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 animated bounceIn">
+                        <h1>100.000 maaltijden</h1>
+                        <p class="font-weight-bold">
+                            De Participatie Keuken zet zich in voor eenzame ouderen om 15 weken lang elke dag een diepvriesmaaltijd te bereiden in de regio Den Haag
+                        </p>
+                        <div class="gfm-embed" data-url="https://www.gofundme.com/f/100000-maaltijden-voor-ouderen/widget/small"></div><script defer src="https://www.gofundme.com/static/js/embed.js"></script>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="text-center container" id="offer-help">
+            <h2>Ik wil helpen!</h2>
+            <p class="subtitle">Hoe kan ik helpen?</p>
             <div class="row">
-                <div class="col-sm-6 animated bounceIn">
-                    <h1>100.000 maaltijden</h1>
-                    <p class="font-weight-bold">
-                        De Participatie Keuken zet zich in voor eenzame ouderen om 15 weken lang elke dag een diepvriesmaaltijd te bereiden in de regio Den Haag
+                <div class="col-sm-4">
+                    <img class="image-block" src="assets/images/volunteer-icon.png" />
+                    <h3>Word vrijwilliger</h3>
+                    <p class="text-justify">
+                        De ambitie van Ben om 100.000 maaltijden te bereiden is niet niks, maar vele handen maken licht werk! De Participatie Keuken is daarom opzoek naar vrijwilligers die in allerlei facetten van het project een steentje willen bijdragen. Meld je snel aan en geef aan waar wij jouw hulp kunnen gebruiken!
                     </p>
-                    <div class="gfm-embed" data-url="https://www.gofundme.com/f/100000-maaltijden-voor-ouderen/widget/small"></div><script defer src="https://www.gofundme.com/static/js/embed.js"></script>
+                    <a target="_blank" href="<?php echo $aanmeldLink;?>" class="btn btn-lg btn-red">
+                        Meld je hier aan
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <div class="image-block"></div>
+                    <h3>Word Donateur</h3>
+                    <p class="text-justify">
+                        Met behulp van jouw donatie kunnen wij het plan verwezenlijken om voor eenzame ouderen diepvriesmaaltijden te bereiden en deze vervolgens bij hen te kunnen langs brengen! Denk bijvoorbeeld aan het inkopen van alle ingrediënten voor de recepten, labels met de bereidingswijze, maar ook het vervoeren van de maaltijden.
+                    </p>
+                    <a target="_blank" class="btn btn-lg btn-bordeaux" href="<?php echo $donateLink; ?>">
+                        Klik hier om te doneren
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <img class="image-block" src="assets/images/bedrijven-icon.png" />
+                    <h3>Bedrijven</h3>
+                    <p class="text-justify">
+                        Als bedrijf bent u onmisbaar in het succes van De Participatie Keuken. Momenteel staan wij voor vele uitdagingen waar wij kennis, producten, ideeën en mensen vanuit het bedrijfsleven hard kunnen gebruiken.
+                    </p>
+                    <a target="_blank" href="<?php echo $bedrijvenLink;?>" class="btn btn-lg btn-blue">
+                        Bedrijven
+                    </a>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="text-center container" id="offer-help">
-        <h2>Ik wil helpen!</h2>
-        <p class="subtitle">Hoe kan ik helpen?</p>
-        <div class="row">
-            <div class="col-sm-4">
-                <img class="image-block" src="assets/images/volunteer-icon.png" />
-                <h3>Word vrijwilliger</h3>
-                <p class="text-justify">
-                    De ambitie van Ben om 100.000 maaltijden te bereiden is niet niks, maar vele handen maken licht werk! De Participatie Keuken is daarom opzoek naar vrijwilligers die in allerlei facetten van het project een steentje willen bijdragen. Meld je snel aan en geef aan waar wij jouw hulp kunnen gebruiken!
-                </p>
-                <a target="_blank" href="<?php echo $aanmeldLink;?>" class="btn btn-lg btn-red">
-                    Meld je hier aan
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <div class="image-block"></div>
-                <h3>Word Donateur</h3>
-                <p class="text-justify">
-                    Met behulp van jouw donatie kunnen wij het plan verwezenlijken om voor eenzame ouderen diepvriesmaaltijden te bereiden en deze vervolgens bij hen te kunnen langs brengen! Denk bijvoorbeeld aan het inkopen van alle ingrediënten voor de recepten, labels met de bereidingswijze, maar ook het vervoeren van de maaltijden.
-                </p>
-                <a target="_blank" class="btn btn-lg btn-bordeaux" href="<?php echo $donateLink; ?>">
-                    Klik hier om te doneren
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <img class="image-block" src="assets/images/bedrijven-icon.png" />
-                <h3>Bedrijven</h3>
-                <p class="text-justify">
-                    Als bedrijf bent u onmisbaar in het succes van De Participatie Keuken. Momenteel staan wij voor vele uitdagingen waar wij kennis, producten, ideeën en mensen vanuit het bedrijfsleven hard kunnen gebruiken.
-                </p>
-                <a target="_blank" href="<?php echo $bedrijvenLink;?>" class="btn btn-lg btn-blue">
-                    Bedrijven
-                </a>
-            </div>
-        </div>
-    </section>
-    <section class="text-justify bg-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3>Het verhaal van Ben</h3>
-                    <p>
-                        Gewoon door samen te koken en daarna aan tafel te gaan bracht Ben Lachhab eenzame ouderen bij elkaar. Toen dit niet meer mogelijk was, ging Ben gelijk aan de slag.
-                    </p>
-                    <p>
-                        Honderdduizend maaltijden voor kwetsbare ouderen wilde hij uitdelen voor al diegenen die nu nergens meer terecht kunnen. Waar hij alleen startte met een bericht op Facebook wordt er op dit moment al met vele partijen besproken hoe dit plan volgende week succesvol van start kan gaan.
-                    </p>
-                    <blockquote>&quot;Ik hoop dat we hier sterker uit komen. Onze krachten bundelen, daar gaat het om!&quot;</blockquote>
-                    <p>
-                        En dat is gebeurd! Shell heeft inmiddels de katering beschikbaar gesteld om dit project te kunnen waarmaken. Verder is gemeente Den Haag ook nauw betrokken om van dit project een succes te maken!
-                    </p>
-                </div>
-                <div class="col-sm-6" id="news-block">
-                    <h3>Ben in het Nieuws</h3>
-                    <ul class="news-items">
-                        <li>
-                            <span class="datum">19:45 18 maart</span>
-                            <p>
-                                Het initiatief van de Haagse restaurant manager Ben Lachhab wordt door steeds meer bedrijven gesteund. Zo kookte het hoofdkantoor van Shell woensdag al haar voorraad voor het bedrijfsrestaurant tot diepvriesmaaltijden. 'Toen we van de actie hoorden, werden we gelijk enthousiast. Dit is een ontzettend mooi initiatief en op deze manier verspillen we geen eten', zegt Michiel van Wolferen, hospitality manager bij Shell.
-                                <a target="_blank" href="https://www.omroepwest.nl/nieuws/4014114/Shell-kookt-diepvriesmaaltijden-voor-kwetsbare-ouderen">
-                                    Lees het hele artikel hier.
-                                </a>
-                            </p>
-                        </li>
-                        <li>
-                            <span class="datum">12:30 18 maart</span>
-                            <p>
-                                Hagenaar Ben brengt duizenden maaltijden thuis bij kwetsbare ouderen
-                                Ben Lachhab bezorgt de komende vijftien weken dankzij giften van sponsoren 100.000 maaltijden bij kwetsbare ouderen. &quot;Uit angst voor het coronavirus durven zij vaak de deur niet uit. En ze hebben toch heus eten nodig.&quot;
-                                <a target="_blank" href="https://www.ad.nl/den-haag/hagenaar-ben-brengt-duizenden-maaltijden-thuis-bij-kwetsbare-ouderen~ab2cee0e/">
-                                    Lees het hele artikel hier.
-                                </a>
-                            </p>
-                        </li>
-                        <li>
-                        <span class="datum">17:30 14 maart
-    </span>
-                            <p>
-                                DEN HAAG - Honderdduizend maaltijden om duizend kwetsbare ouderen vijftien weken lang van een maaltijd te voorzien, voor het geval dat Nederland op slot gaat vanwege het coronavirus. Hagenaar Ben Lachhab is druk met het uitwerken van zijn plan. 'We moeten nu bewijzen dat we er als maatschappij voor elkaar zijn.
-                                <a target="_blank" href="https://www.omroepwest.nl/nieuws/4011604/Hagenaar-wil-tijdens-coronacrisis-100-000-maaltijden-invriezen-voor-ouderen">
-                                    Lees het hele artikel hier.
-                                </a>
-                            </p>
-                        </li>
-                    </ul>
+        </section>
+        <section class="text-justify bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h3>Het verhaal van Ben</h3>
+                        <p>
+                            Gewoon door samen te koken en daarna aan tafel te gaan bracht Ben Lachhab eenzame ouderen bij elkaar. Toen dit niet meer mogelijk was, ging Ben gelijk aan de slag.
+                        </p>
+                        <p>
+                            Honderdduizend maaltijden voor kwetsbare ouderen wilde hij uitdelen voor al diegenen die nu nergens meer terecht kunnen. Waar hij alleen startte met een bericht op Facebook wordt er op dit moment al met vele partijen besproken hoe dit plan volgende week succesvol van start kan gaan.
+                        </p>
+                        <blockquote>&quot;Ik hoop dat we hier sterker uit komen. Onze krachten bundelen, daar gaat het om!&quot;</blockquote>
+                        <p>
+                            En dat is gebeurd! Shell heeft inmiddels de katering beschikbaar gesteld om dit project te kunnen waarmaken. Verder is gemeente Den Haag ook nauw betrokken om van dit project een succes te maken!
+                        </p>
+                    </div>
+                    <div class="col-sm-6" id="news-block">
+                        <h3>Ben in het Nieuws</h3>
+                        <ul class="news-items">
+                            <li>
+                                <span class="datum">19:45 18 maart</span>
+                                <p>
+                                    Het initiatief van de Haagse restaurant manager Ben Lachhab wordt door steeds meer bedrijven gesteund. Zo kookte het hoofdkantoor van Shell woensdag al haar voorraad voor het bedrijfsrestaurant tot diepvriesmaaltijden. 'Toen we van de actie hoorden, werden we gelijk enthousiast. Dit is een ontzettend mooi initiatief en op deze manier verspillen we geen eten', zegt Michiel van Wolferen, hospitality manager bij Shell.
+                                    <a target="_blank" href="https://www.omroepwest.nl/nieuws/4014114/Shell-kookt-diepvriesmaaltijden-voor-kwetsbare-ouderen">
+                                        Lees het hele artikel hier.
+                                    </a>
+                                </p>
+                            </li>
+                            <li>
+                                <span class="datum">12:30 18 maart</span>
+                                <p>
+                                    Hagenaar Ben brengt duizenden maaltijden thuis bij kwetsbare ouderen
+                                    Ben Lachhab bezorgt de komende vijftien weken dankzij giften van sponsoren 100.000 maaltijden bij kwetsbare ouderen. &quot;Uit angst voor het coronavirus durven zij vaak de deur niet uit. En ze hebben toch heus eten nodig.&quot;
+                                    <a target="_blank" href="https://www.ad.nl/den-haag/hagenaar-ben-brengt-duizenden-maaltijden-thuis-bij-kwetsbare-ouderen~ab2cee0e/">
+                                        Lees het hele artikel hier.
+                                    </a>
+                                </p>
+                            </li>
+                            <li>
+                            <span class="datum">17:30 14 maart
+        </span>
+                                <p>
+                                    DEN HAAG - Honderdduizend maaltijden om duizend kwetsbare ouderen vijftien weken lang van een maaltijd te voorzien, voor het geval dat Nederland op slot gaat vanwege het coronavirus. Hagenaar Ben Lachhab is druk met het uitwerken van zijn plan. 'We moeten nu bewijzen dat we er als maatschappij voor elkaar zijn.
+                                    <a target="_blank" href="https://www.omroepwest.nl/nieuws/4011604/Hagenaar-wil-tijdens-coronacrisis-100-000-maaltijden-invriezen-voor-ouderen">
+                                        Lees het hele artikel hier.
+                                    </a>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="text-center" id="join-the-team">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <h2>Join the team</h2>
-                    <p>
-                        Wij zijn op zoek naar mensen die kunnen helpen om van De Participatie Keuken een groot succes te maken.</p>
-                    <p>Hiervoor hebben wij ook specifieke hulp nodig! Wij zijn momenteel op zoek naar vrijwilligers die zich willen inzetten als de rechterhand van Ben, de initiatiefnemer. Je zal je bezig houden met zowel <b>organisatorische zaken</b> als de <b>communicatie</b>.</p>
-                    <p>Ben of ken jij de persoon die we zoeken? Stuur dan een mail naar
-                        <a target="_blank" class="font-weight-bold" href="mailto:<?php echo $infoEmail; ?>"><?php echo $infoEmail; ?></a>
-                    </p>
+        </section>
+        <section class="text-center" id="join-the-team">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 offset-sm-3">
+                        <h2>Join the team</h2>
+                        <p>
+                            Wij zijn op zoek naar mensen die kunnen helpen om van De Participatie Keuken een groot succes te maken.</p>
+                        <p>Hiervoor hebben wij ook specifieke hulp nodig! Wij zijn momenteel op zoek naar vrijwilligers die zich willen inzetten als de rechterhand van Ben, de initiatiefnemer. Je zal je bezig houden met zowel <b>organisatorische zaken</b> als de <b>communicatie</b>.</p>
+                        <p>Ben of ken jij de persoon die we zoeken? Stuur dan een mail naar
+                            <a target="_blank" class="font-weight-bold" href="mailto:<?php echo $infoEmail; ?>"><?php echo $infoEmail; ?></a>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </main>
     <footer class="bg-light">
         <div class="container">
             <div class="row">
@@ -190,9 +193,14 @@ $infoEmail = "info@participatiekeuken.nl";
                     <p>
                         <span class="font-weight-bold">Logistiek</span>
                         <br/>
-                        Email: <a target="_blank" href="mailto:actieteam@participatiekeuken.nl">actieteam@participatiekeuken.nl</a>
+                        Email: <a target="_blank" href="mailto:<?php echo $actieTeamEmail; ?>"><?php echo $actieTeamEmail; ?></a>
                     </p>
-                    <img src="./assets/images/logo-big.png" />
+                    <a href="https://www.linkedin.com/company/de-participatiekeuken/">
+                        <img src="./assets/images/social-linkedin-circle-512.png" class="social-logo" />
+                    </a>
+                    <br />
+                    <br />
+                    <img src="./assets/images/logo-big.png" class="logo" />
                 </div>
             </div>
         </div>
